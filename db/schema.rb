e6351dfff2f9cbdde55cb482126b8e591885e53b
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-03-06 21:09:02 +0200

    Linking user to created links

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_182705) do
+ActiveRecord::Schema.define(version: 2021_03_06_185956) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_03_06_182705) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "links", "users"
 end
