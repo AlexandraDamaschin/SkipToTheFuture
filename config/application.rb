7536da4e92c64d198487d9d6e9061b9c9ccc577d
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2021-05-07 09:48:54 +0300

    Add db to production

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module SkipApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    # Initialize configuration defaults for originally generated Rails version.manifest.js
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
